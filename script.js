f0b7826dd6973314359ee92f86fe9d932b21f050
romeirodv-lgtm <romeirodv@gmail.com>
2025-10-03 02:49:54 -0300

    script.js

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
-// MENU MOBILE
-const mobileMenu = document.getElementById('mobileMenu');
-const mainNav = document.getElementById('mainNav');
 
-mobileMenu.addEventListener('click', () => {
-  mainNav.classList.toggle('active');
+const modal = document.getElementById('modal');
+const modalTitle = document.getElementById('modalTitle');
+const modalDesc = document.getElementById('modalDesc');
+const closeModal = document.getElementById('closeModal');
+const cards = document.querySelectorAll('.card');
+
+cards.forEach(card => {
+    card.addEventListener('click', () => {
+        modalTitle.textContent = card.querySelector('h3').textContent;
+        modalDesc.textContent = card.getAttribute('data-description');
+        modal.style.display = 'flex';
+    });
 });
+
+closeModal.addEventListener('click', () => { modal.style.display = 'none'; });
+window.addEventListener('click', (e) => { if(e.target == modal) modal.style.display = 'none'; });
